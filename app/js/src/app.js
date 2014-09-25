579d524bfe3c3fd5dfe698e3c07458c38d8621e8
Koen Vendrik <k.vendrik@gmail.com>
2014-09-25 17:14:01 +0200

    Implemented URL hash option
    Implemented URL hash option to go to specific slide and step

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -3,6 +3,22 @@
   var slides = document.getElementsByClassName('slide'),
       currSlideIdx = 0,
 
+      makeStepsVisible = function(slideIdx, stepsAmount){
+        var currSlide = slides[slideIdx],
+            steps = currSlide.getElementsByClassName('step');
+
+        for(var i = 0, stepsCount = steps.length; i < stepsCount; i++){
+
+          var curr = B(steps[i]);
+          if( i <= stepsAmount ){
+            curr.addClass('done');
+          } else {
+            curr.removeClass('done');
+          }
+
+        }
+      },
+
       checkNextStep = function(slideIdx){
         var currSlide = slides[slideIdx],
             steps = currSlide.getElementsByClassName('step');
@@ -40,7 +56,6 @@
       },
 
       switchSlide = function(currSlideIdx, newSlideIdx){
-        console.log(newSlideIdx);
          B(slides[ currSlideIdx ]).removeClass('visible');
          B(slides[ newSlideIdx ]).addClass('visible');
       },
@@ -61,8 +76,26 @@
         return prevSlideIdx;
       };
 
-  //make first slide visible
-  B(slides[currSlideIdx]).addClass('visible');
+  //if hash available
+  //extract slideIdx and stepIdx
+  //and go to slideIdx and stepIdx
+  if(location.hash !== ''){
+
+    var hash = location.hash,
+        slideIdx = Number(hash.match(/slide\-(\d+)/)[1]),
+
+        stepMatch = hash.match(/step\-(\d+)/),
+        stepIdx = stepMatch ? Number(stepMatch[1]) : 0;
+
+    switchSlide(currSlideIdx, slideIdx);
+    makeStepsVisible(slideIdx, stepIdx);
+
+    currSlideIdx = slideIdx;
+
+  } else {
+    //make first slide visible
+    B(slides[currSlideIdx]).addClass('visible');
+  }
 
   document.addEventListener('keyup', function(e){
 
