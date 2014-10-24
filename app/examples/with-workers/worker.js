047a205b9b15e8d556d3731451de92ca789261b4
Koen Vendrik <k.vendrik@gmail.com>
2014-10-24 15:36:34 +0200

    Example presentation: minor changes, removed fallback-library example, added challenge

--- app/examples/with-workers/worker.js
+++ app/examples/with-workers/worker.js
@@ -1,7 +1,7 @@
 var genRandStr = function(iterations){
        var str = '';
        for(var i = 0; i < iterations; i++){
-         //5 chars per iteration
+         //11 chars per iteration
          str += Math.random().toString(36).substring(7);
        }
       return str;
