fcece35e5c23476a9b729a2a91b2ad7181c5887e
Koen Vendrik <k.vendrik@gmail.com>
2015-05-13 17:10:26 +0200

    Server now serves presentation files

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -1,173 +1,186 @@
 (function(){
 
-  var slides = document.getElementsByClassName('slide'),
-      currSlideIdx = 0,
+    var slides = document.getElementsByClassName('slide'),
+        currSlideIdx = 0,
 
-      makeStepsVisible = function(slideIdx, stepsAmount){
-        var currSlide = slides[slideIdx],
-            steps = currSlide.getElementsByClassName('step');
+        makeStepsVisible = function(slideIdx, stepsAmount){
+            var currSlide = slides[slideIdx],
+                steps = currSlide.getElementsByClassName('step');
 
-        for(var i = 0, stepsCount = steps.length; i < stepsCount; i++){
+            for(var i = 0, stepsCount = steps.length; i < stepsCount; i++){
 
-          var curr = B(steps[i]);
-          if( i <= stepsAmount ){
-            curr.addClass('done');
-          } else {
-            curr.removeClass('done');
-          }
+                var curr = B(steps[i]);
+                if( i <= stepsAmount ){
+                    curr.addClass('done');
+                } else {
+                    curr.removeClass('done');
+                }
 
-        }
-      },
+            }
+        },
 
-      checkNextStep = function(slideIdx){
-        var currSlide = slides[slideIdx],
-            steps = currSlide.getElementsByClassName('step');
+        checkNextStep = function(slideIdx){
+            var currSlide = slides[slideIdx],
+                steps = currSlide.getElementsByClassName('step');
 
-        for(var i = 0, stepsCount = steps.length; i < stepsCount; i++){
+            for(var i = 0, stepsCount = steps.length; i < stepsCount; i++){
 
-          var curr = B(steps[i]);
-          if(!curr.hasClass('done')){
-            //step found thats not done
-            curr.addClass('done');
-            if(history.pushState) history.pushState([(slideIdx+1), (i+1)], 'Slide: '+(slideIdx+1), '#slide-'+(slideIdx+1)+'&step-'+(i+1));
-            return { result: false, newStepIdx: i };
-          }
+                var curr = B(steps[i]);
+                if(!curr.hasClass('done')){
+                    //step found thats not done
+                    curr.addClass('done');
 
-        }
+                    if(history.pushState){
+                        history.pushState([(slideIdx+1), (i+1)], 'Slide: '+(slideIdx+1), '#slide-'+(slideIdx+1)+'&step-'+(i+1));
+                    }
 
-        return { result: true };
-      },
+                    return { result: false, newStepIdx: i };
+                }
 
-      checkPrevStep = function(slideIdx){
-        var currSlide = slides[slideIdx],
-            steps = currSlide.getElementsByClassName('step');
+            }
 
-        for(var i = steps.length-1; i >= 0; i--){
+            return { result: true };
+        },
 
-          var curr = B(steps[i]);
-          if(curr.hasClass('done')){
-            //step found thats done
-            curr.removeClass('done');
-            if(history.pushState) history.pushState([(slideIdx+1), i], 'Slide: '+(slideIdx+1), '#slide-'+(slideIdx+1)+(i !== 0 ? '&step-'+i : ''));
-            return { result: false, newStepIdx: i-1 };
-          }
+        checkPrevStep = function(slideIdx){
+            var currSlide = slides[slideIdx],
+                steps = currSlide.getElementsByClassName('step');
 
-        }
+            for(var i = steps.length-1; i >= 0; i--){
+                var curr = B(steps[i]);
 
-        return { result: true };
-      },
+                if(curr.hasClass('done')){
+                    //step found thats done
+                    curr.removeClass('done');
+                    if(history.pushState){
+                        history.pushState([(slideIdx+1), i], 'Slide: '+(slideIdx+1), '#slide-'+(slideIdx+1)+(i !== 0 ? '&step-'+i : ''));
+                    }
+                    return { result: false, newStepIdx: i-1 };
+                }
+            }
 
-      switchSlide = function(currSlideIdx, newSlideIdx){
-         B(slides[ currSlideIdx ]).removeClass('visible');
-         B(slides[ newSlideIdx ]).addClass('visible');
-      },
+            return { result: true };
+        },
 
-      nextSlide = function(currSlideIdx){
-        var nextSlideIdx = slides[currSlideIdx+1] !== undefined ? currSlideIdx+1 : 0;
+        switchSlide = function(currSlideIdx, newSlideIdx){
+            B(slides[ currSlideIdx ]).removeClass('visible');
+            B(slides[ newSlideIdx ]).addClass('visible');
+        },
 
-        switchSlide(currSlideIdx, nextSlideIdx);
-        if(history.pushState) history.pushState([(nextSlideIdx+1), 0], 'Slide: '+(nextSlideIdx+1), '#slide-'+(nextSlideIdx+1));
+        nextSlide = function(currSlideIdx){
+            var nextSlideIdx = slides[currSlideIdx+1] !== undefined ? currSlideIdx+1 : 0;
 
-        return nextSlideIdx;
-      },
+            switchSlide(currSlideIdx, nextSlideIdx);
+            if(history.pushState){
+                history.pushState([(nextSlideIdx+1), 0], 'Slide: '+(nextSlideIdx+1), '#slide-'+(nextSlideIdx+1));
+            }
 
-      prevSlide = function(currSlideIdx){
-        var prevSlideIdx = slides[currSlideIdx-1] !== undefined ? currSlideIdx-1 : slides.length-1;
+            return nextSlideIdx;
+        },
 
-        switchSlide(currSlideIdx, prevSlideIdx);
-        if(history.pushState) history.pushState([(prevSlideIdx+1), 0], 'Slide: '+(prevSlideIdx+1), '#slide-'+(prevSlideIdx+1));
+        prevSlide = function(currSlideIdx){
+            var prevSlideIdx = slides[currSlideIdx-1] !== undefined ? currSlideIdx-1 : slides.length-1;
 
-        return prevSlideIdx;
-      },
+            switchSlide(currSlideIdx, prevSlideIdx);
+            if(history.pushState){
+                history.pushState([(prevSlideIdx+1), 0], 'Slide: '+(prevSlideIdx+1), '#slide-'+(prevSlideIdx+1));
+            }
 
-      goToState = function(slideIdx, stepIdx, pushHistory){
+            return prevSlideIdx;
+        },
 
-        if(pushHistory === undefined){
-          pushHistory = true;
-        }
+        goToState = function(slideIdx, stepIdx, pushHistory){
+            if(pushHistory === undefined){
+                pushHistory = true;
+            }
 
-        switchSlide(currSlideIdx, slideIdx);
+            switchSlide(currSlideIdx, slideIdx);
 
-        if(stepIdx !== undefined && stepIdx !== null){
-          makeStepsVisible(slideIdx, stepIdx);
-        }
+            if(stepIdx !== undefined && stepIdx !== null){
+                makeStepsVisible(slideIdx, stepIdx);
+            }
 
-        if(history.pushState && pushHistory){
-          history.pushState([(slideIdx+1), i], 'Slide: '+(slideIdx+1), '#slide-'+(slideIdx+1)+(stepIdx !== undefined && stepIdx !== null && stepIdx > -1 ? '&step-'+(stepIdx+1) : ''));
-        }
+            if(history.pushState && pushHistory){
+                history.pushState([(slideIdx+1), i], 'Slide: '+(slideIdx+1), '#slide-'+(slideIdx+1)+(stepIdx !== undefined && stepIdx !== null && stepIdx > -1 ? '&step-'+(stepIdx+1) : ''));
+            }
 
-        currSlideIdx = slideIdx;
+            currSlideIdx = slideIdx;
+        },
 
-      },
-      toNext = function(){
-        var checkStep = checkNextStep(currSlideIdx);
-        if(checkStep.result){
-          currSlideIdx = nextSlide(currSlideIdx);
-        }
-        socket.emit('change', [currSlideIdx, checkStep.newStepIdx]);
-      },
-      toPrev = function(){
-        var checkStep = checkPrevStep(currSlideIdx);
-        if(checkStep.result){
-          currSlideIdx = prevSlide(currSlideIdx);
-        }
-        socket.emit('change', [currSlideIdx, checkStep.newStepIdx]);
-      };
+        toNext = function(){
+            var checkStep = checkNextStep(currSlideIdx);
 
-  //if hash available
-  //extract slideIdx and stepIdx
-  //and go to slideIdx and stepIdx
-  if(location.hash !== ''){
+            if(checkStep.result){
+                currSlideIdx = nextSlide(currSlideIdx);
+            }
 
-    var hash = location.hash,
-        slideIdx = Number(hash.match(/slide\-(\d+)/)[1])-1,
+            socket.emit('change', [currSlideIdx, checkStep.newStepIdx]);
+        },
 
-        stepMatch = hash.match(/step\-(\d+)/),
-        stepIdx = stepMatch ? Number(stepMatch[1])-1 : undefined;
+        toPrev = function(){
+            var checkStep = checkPrevStep(currSlideIdx);
 
-    goToState(slideIdx, stepIdx);
+            if(checkStep.result){
+                currSlideIdx = prevSlide(currSlideIdx);
+            }
 
-  } else {
-    //make first slide visible
-    B(slides[currSlideIdx]).addClass('visible');
-  }
+            socket.emit('change', [currSlideIdx, checkStep.newStepIdx]);
+        };
 
-  //init sockets
-  var socket = io(':3000');
+    //if hash available
+    //extract slideIdx and stepIdx
+    //and go to slideIdx and stepIdx
+    if(location.hash !== ''){
 
-  //on change go to corresponding slide
-  //trigger change on every change made to slides
-  socket.on('change', function(idxs){
-    goToState(idxs[0], idxs[1]);
-  });
+        var hash = location.hash,
+            slideIdx = Number(hash.match(/slide\-(\d+)/)[1])-1,
 
-  //listen for back or forward button on browser
-  window.addEventListener('popstate', function(e){
-    var state = e.state,
-        newSlideIdx,
-        newStepIdx;
+            stepMatch = hash.match(/step\-(\d+)/),
+            stepIdx = stepMatch ? Number(stepMatch[1])-1 : undefined;
 
-    if(state !== null) {
-      newSlideIdx = state[0]-1;
-      newStepIdx = state[1]-1;
+        goToState(slideIdx, stepIdx);
+
     } else {
-      newSlideIdx = newStepIdx = 0;
+        //make first slide visible
+        B(slides[currSlideIdx]).addClass('visible');
     }
 
-    goToState(newSlideIdx, newStepIdx, false);
-    socket.emit('change', [newSlideIdx, newStepIdx]);
-  });
+    //init sockets
+    var socket = io(':3000');
 
-  document.addEventListener('keyup', function(e){
-    if(e.keyCode === 39 || e.keyCode === 32){
-      //next (arrow or space)
-      toNext();
-    } else if(e.keyCode === 37){
-      //prev
-      toPrev();
-    }
-  }, false);
+    //on change go to corresponding slide
+    //trigger change on every change made to slides
+    socket.on('change', function(idxs){
+        goToState(idxs[0], idxs[1]);
+    });
 
-  document.addEventListener('ontouchstart' in window ? 'touchstart' : 'click', toNext, false);
+    //listen for back or forward button on browser
+    window.addEventListener('popstate', function(e){
+        var state = e.state,
+            newSlideIdx,
+            newStepIdx;
+
+        if(state !== null) {
+            newSlideIdx = state[0]-1;
+            newStepIdx = state[1]-1;
+        } else {
+            newSlideIdx = newStepIdx = 0;
+        }
+
+        goToState(newSlideIdx, newStepIdx, false);
+        socket.emit('change', [newSlideIdx, newStepIdx]);
+    });
+
+    document.addEventListener('keyup', function(e){
+        if(e.keyCode === 39 || e.keyCode === 32){
+            //next (arrow or space)
+            toNext();
+        } else if(e.keyCode === 37){
+            //prev
+            toPrev();
+        }
+    }, false);
+
+    document.addEventListener('ontouchstart' in window ? 'touchstart' : 'click', toNext, false);
 
 }());
--- server.js
+++ server.js
@@ -1,14 +1,57 @@
-var app = require('express')(),
+var express = require('express'),
+    app = express(),
 	http = require('http').Server(app),
 	io = require('socket.io')(http);
 
-io.on('connection', function(socket){
-  socket.on('change', function(idxs){
-    console.log(idxs);
-    socket.broadcast.emit('change', idxs);
-  });
+var options = {},
+    defaults = {
+        port: 3000
+    };
+
+var extend = function(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+};
+
+//parse command line args
+process.argv.forEach(function(val, idx){
+    if(val.indexOf('--') === 0){
+        //is flag
+        var parts = val.replace('--', '').split('='),
+            val = parts[1];
+
+        if(typeof val === 'undefined'){
+            //is undefined
+            val = true;
+        } else if(!isNaN(val)){
+            //is number
+            val = Number(val);
+        }
+
+        options[parts[0]] = val;
+    }
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+//merge default and options
+var options = extend({}, defaults, options);
+
+//listen for connections
+io.on('connection', function(socket){
+    socket.on('change', function(idxs){
+        console.log(idxs);
+        socket.broadcast.emit('change', idxs);
+    });
+});
+
+//serve static files on req
+app.use('/', express.static(__dirname+'/app'));
+
+//listen on port
+http.listen(options.port, function() {
+    console.log('listening on *:'+options.port);
 });
